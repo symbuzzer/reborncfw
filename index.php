5583ce9dc5a12a7d165ffc7b065e47512763f874
Ali Beyaz <symbuzzer@users.noreply.github.com>
2025-11-22 19:40:42 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="tr" xml:lang="tr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="1;URL=https://avalibeyaz.com/s3" />
+<meta http-equiv="cache-control" content="no-store, no-cache, must-revalidate" />
+<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<title>...</title>
+<style type="text/css">
+html, body {
+  margin:0;
+  padding:0;
+  height:100%;
+  background-color:#000000;
+  text-align:center;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
